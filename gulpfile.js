e397d0fb229c765984fb9c9a89e973783fc0c33a
Outvi V <git@outv.im>
2025-11-02 21:30:40 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp')
+const toml = require('gulp-toml')
+const rename = require('gulp-rename')
+const fs = require('fs')
+const htmlmin = require('gulp-htmlmin')
+const cleanCSS = require('gulp-clean-css')
+const nunjucksRender = require('gulp-nunjucks-render')
+
+gulp.task('toml', () => {
+  return gulp
+    .src('data.toml')
+    .pipe(toml())
+    .pipe(rename({ extname: '.json' }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-css', () => {
+  return gulp.src('src/style.css').pipe(cleanCSS()).pipe(gulp.dest('dist'))
+})
+
+gulp.task('generate-html', () => {
+  const data = JSON.parse(fs.readFileSync('dist/data.json', 'utf-8'))
+  return gulp
+    .src('src/templates/*.njk')
+    .pipe(
+      nunjucksRender({
+        data: data,
+        path: ['src/templates', 'dist'],
+      })
+    )
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('minify-html', () => {
+  return gulp
+    .src('dist/index.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task(
+  'build-html',
+  gulp.series('minify-css', 'generate-html', 'minify-html')
+)
+
+gulp.task('default', gulp.series('toml', 'build-html'))
+
+gulp.task('watch', () => {
+  gulp.watch('data.toml', gulp.series('default'))
+  gulp.watch('src/style.css', gulp.series('build-html'))
+  gulp.watch('src/templates/**/*.njk', gulp.series('build-html'))
+})
